ca76c66b8e192422d97398154557722f2be0509c
Jory Roberts <robertsjory13@gmail.com>
2023-10-15 12:51:14 -0500

    complete lesson01

--- build/js/main.js
+++ build/js/main.js
@@ -2,3 +2,7 @@
 let username = 'Madi';
 console.log(username);
 let a = 12;
+let b = 6;
+let c = 2;
+console.log(a / b);
+console.log(c * b);
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 let username = 'Madi';
 console.log(username);
 
-let a = 12;
+let a: number = 12;
+let b: number = 6;
+let c: number = 2;
+
+console.log(a / b);
+
+console.log(c * b);
