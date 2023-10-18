b36f7e9836b7b1f8999679b6bb5c0f32ff0abba6
Jory Roberts <robertsjory13@gmail.com>
2023-10-18 11:28:28 -0500

    initial functions branch commit

--- project/build/js/main.js
+++ project/build/js/main.js
@@ -1,52 +1,14 @@
 "use strict";
-let stringArr = ['one', 'hey', 'Amber'];
-let guitars = ['Strat', 'Les Paul', 5150];
-let mixedData = ['EVH', 1984, true];
-stringArr[0] = 'Madi';
-stringArr.push('welcome');
-guitars[0] = 1984;
-guitars.unshift('Julie');
-let test = [];
-let bands = [];
-bands.push('Van Halen');
-//Tuple
-let myTuple = ['Amber', 34, true];
-let mixed = ['John', 1, false];
-//Objects
-let myObj;
-myObj = [];
-console.log(typeof myObj);
-myObj = bands;
-const exammpleObj = {
-    prop1: 'Jory',
-    prop2: true,
+// Literal Types
+let myName = 'Jory';
+let userName;
+userName = 'Amber';
+//Functions
+const add = (a, b) => a + b;
+const logMsg = (message) => console.log(message);
+logMsg('Hello');
+logMsg(add(2, 3));
+logMsg(add(2, 5));
+let subtract = function (c, d) {
+    return c - d;
 };
-let evh = {
-    name: 'Eddie',
-    active: false,
-    albums: [1984, 5150, 'OU812'],
-};
-let jp = {
-    name: 'Jimmy',
-    active: true,
-    albums: ['I', 'II', 'IV'],
-};
-evh = jp;
-const greetGuitarist = (guitarist) => {
-    if (guitarist.name) {
-        return `Hello ${guitarist.name.toUpperCase()}`;
-    }
-    return 'Hello!';
-};
-console.log(greetGuitarist(jp));
-//Enums
-//Not a type-level addition to JS
-var Grade;
-(function (Grade) {
-    Grade[Grade["U"] = 1] = "U";
-    Grade[Grade["D"] = 2] = "D";
-    Grade[Grade["C"] = 3] = "C";
-    Grade[Grade["B"] = 4] = "B";
-    Grade[Grade["A"] = 5] = "A";
-})(Grade || (Grade = {}));
-console.log(Grade.U);
--- project/src/main.ts
+++ project/src/main.ts
@@ -1,76 +1,30 @@
-let stringArr = ['one', 'hey', 'Amber'];
+//type aliases
+type stringOrNumber = string | number;
 
-let guitars = ['Strat', 'Les Paul', 5150];
-
-let mixedData = ['EVH', 1984, true];
-
-stringArr[0] = 'Madi';
-stringArr.push('welcome');
-
-guitars[0] = 1984;
-guitars.unshift('Julie');
-
-let test = [];
-let bands: string[] = [];
-bands.push('Van Halen');
-
-//Tuple
-let myTuple: [string, number, boolean] = ['Amber', 34, true];
-
-let mixed = ['John', 1, false];
-
-//Objects
-let myObj: object;
-myObj = [];
-console.log(typeof myObj);
-
-myObj = bands;
-
-const exammpleObj = {
-  prop1: 'Jory',
-  prop2: true,
-};
-
-//Types
-//Interfaces similar structure, interfaces used more with classes
+type stringOrNumberArray = (string | number)[];
 interface Guitarist {
   name?: string;
   active: boolean;
-  albums: (string | number)[];
+  albums: stringOrNumberArray;
 }
 
-let evh: Guitarist = {
-  name: 'Eddie',
-  active: false,
-  albums: [1984, 5150, 'OU812'],
+type UserId = stringOrNumber;
+
+// Literal Types
+let myName = 'Jory';
+
+let userName: 'Jory' | 'Amber';
+userName = 'Amber';
+
+//Functions
+const add = (a: number, b: number): Number => a + b;
+
+const logMsg = (message: any): void => console.log(message);
+
+logMsg('Hello');
+logMsg(add(2, 3));
+logMsg(add(2, 5));
+
+let subtract = function (c: number, d: number): number {
+  return c - d;
 };
-
-let jp: Guitarist = {
-  name: 'Jimmy',
-  active: true,
-  albums: ['I', 'II', 'IV'],
-};
-
-evh = jp;
-
-const greetGuitarist = (guitarist: Guitarist) => {
-  if (guitarist.name) {
-    return `Hello ${guitarist.name.toUpperCase()}`;
-  }
-  return 'Hello!';
-};
-
-console.log(greetGuitarist(jp));
-
-//Enums
-//Not a type-level addition to JS
-
-enum Grade {
-  U = 1,
-  D,
-  C,
-  B,
-  A,
-}
-
-console.log(Grade.U);
