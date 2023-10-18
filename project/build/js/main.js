01fd1073b61a3175069202b665156e1d5c476d32
Jory Roberts <robertsjory13@gmail.com>
2023-10-18 11:51:26 -0500

    finishes with functions

--- project/build/js/main.js
+++ project/build/js/main.js
@@ -12,3 +12,42 @@ logMsg(add(2, 5));
 let subtract = function (c, d) {
     return c - d;
 };
+let multiply = function (c, d) {
+    return c * d;
+};
+logMsg(multiply(2, 2));
+// optional parameters
+const addAll = (a, b, c) => {
+    if (typeof c !== 'undefined') {
+        return a + b + c;
+    }
+    return a + b;
+};
+const sumAll = (a, b, c = 2) => a + b + c;
+logMsg(addAll(2, 3, 2));
+logMsg(addAll(2, 3));
+logMsg(sumAll(2, 3));
+//Rest parameters
+const total = (...nums) => {
+    return nums.reduce((prev, curr) => prev + curr);
+};
+logMsg(total(1, 2, 3, 4));
+//Never type: endless loops / throw errors
+const createError = (errorMsg) => {
+    throw new Error(errorMsg);
+};
+const inifinite = () => {
+    let i = 1;
+    while (true) {
+        i++;
+        if (i > 100)
+            break; //without this, this is a never type
+    }
+};
+const createNumberOrString = (value) => {
+    if (typeof value === 'string')
+        return 'string';
+    if (typeof value === 'number')
+        return 'number';
+    return createError('This should never happen');
+};
--- project/src/main.ts
+++ project/src/main.ts
@@ -28,3 +28,55 @@ logMsg(add(2, 5));
 let subtract = function (c: number, d: number): number {
   return c - d;
 };
+
+// type mathFunction = (a: number, b: number) => number;
+// interfaces are classes / functions type aliases
+interface mathFunction {
+  (a: number, b: number): number;
+}
+
+let multiply: mathFunction = function (c, d) {
+  return c * d;
+};
+
+logMsg(multiply(2, 2));
+
+// optional parameters
+const addAll = (a: number, b: number, c?: number) => {
+  if (typeof c !== 'undefined') {
+    return a + b + c;
+  }
+  return a + b;
+};
+
+const sumAll = (a: number, b: number, c: number = 2) => a + b + c;
+
+logMsg(addAll(2, 3, 2));
+logMsg(addAll(2, 3));
+logMsg(sumAll(2, 3));
+
+//Rest parameters
+const total = (...nums: number[]): number => {
+  return nums.reduce((prev, curr) => prev + curr);
+};
+
+logMsg(total(1, 2, 3, 4));
+
+//Never type: endless loops / throw errors
+const createError = (errorMsg: string) => {
+  throw new Error(errorMsg);
+};
+
+const inifinite = () => {
+  let i: number = 1;
+  while (true) {
+    i++;
+    if (i > 100) break; //without this, this is a never type
+  }
+};
+
+const createNumberOrString = (value: number | string): string => {
+  if (typeof value === 'string') return 'string';
+  if (typeof value === 'number') return 'number';
+  return createError('This should never happen');
+};
